29b9513537ee3e121415947f6309be8d96b4fe27
roshna <RoshnaLakshmiChandrasekaran@dal.ca>
2019-02-15 10:57:44 -0800

    5 functions

--- opentask1.py
+++ opentask1.py
@@ -1,5 +1,5 @@
 inputFilename =  "Book1.txt"
-def unique_file(input_filename, output_filename):
+def unique_words(input_filename, output_filename):
     input_file = open(input_filename, 'r')
     file_contents = input_file.read()
     input_file.close()
@@ -10,7 +10,17 @@ def unique_file(input_filename, output_filename):
     for word in word_list:
         if word not in word_list:
             file.write(str(word) + "\n")
-    file.close()
+file.close()
+
+def count_the article[word list]:
+ for word in file.read().split():
+    if word not in wordcount:
+        wordcount[word] = 1
+    else:
+        wordcount[word] += 1
+print(word)
+file.close()
+    
 
 def sorted_words[word list]:
     result = []
@@ -27,5 +37,14 @@ def character_word_count:
       else:
          wordcount[word] += 1
 print (word,wordcount)
+file.close()
+
+def starts_with_vow[word list]:
+    while index < len(word list):
+        if word list[index] in vowels:
+            vowels += 1
+            index += 1
+
+print(word,wordcount)
 file.close()
 
